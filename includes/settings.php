51f86bff412bc8a9f6439e2990c48255bee9bc49
Christian Neumann <psychedelic.system@gmail.com>
2023-10-12 10:50:03 +0200

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -9,24 +9,21 @@ class Settings
   private $API;
 
   public $valid_key = false;
-  public function __construct()
-  {
+  public function __construct() {
     $this->add_default_options();
     $this->API = new API();
     add_action('admin_menu', [$this, 'page_init']);
     //add_action('admin_menu', [$this, 'menu_init']);
 
   }
-  private function add_default_options()
-  {
+  private function add_default_options() {
     $default_options = array();
     $default_options['api_key'] = '';
 
     add_option(self::OPTIONS_BASE_NAME, $default_options);
   }
 
-  public function page_init()
-  {
+  public function page_init() {
     add_options_page(
       'Reiseinspirationen',
       // page_title
@@ -72,21 +69,21 @@ class Settings
   /**
    * CALLBACKS
    */
-  public function section_info_callback()
-  {
+  public function section_info_callback() {
+    $akzent_point_of_interest_options = get_option('akzent_point_of_interest_options');
     ?>
-    <p>Hier muss zum abrufen der Reiseinspirationen der benötigte Schlüssel eingebenen werden</p>
-    <p>Den Schlüssel erhalten Sie von der AKZENT Zentrale</p>
-    <p>
-      <a href="tel:+4905321759140">+49 5321 75 91 - 40</a>
-    </p>
-    <p>
-      <a href="mailto:info@akzent.de">info@akzent.de</a>
-    </p>
+      <p>Hier muss zum abrufen der Reiseinspirationen der benötigte Schlüssel eingebenen werden</p>
+      <p>Den Schlüssel erhalten Sie von der AKZENT Zentrale</p>
+      <p><a href="tel:+4905321759140">+49 5321 75 91 - 40</a></p>
+      <p><a href="mailto:info@akzent.de">info@akzent.de</a></p>
+      <?php if( isset($akzent_point_of_interest_options['api_key']) === true ) : ?>
+        <h4 style="margin-top: 25px">Shortcodes</h4>
+        <p>[akzent_points_of_interest_grid_short]</p>
+        <p>[akzent_points_of_interest_slider_short]</p>
+      <?php endif ?>
     <?php
   }
-  public function api_key_callback()
-  {
+  public function api_key_callback() {
     $akzent_point_of_interest_options = get_option('akzent_point_of_interest_options');
     printf(
       '<input class="regular-text" type="text" name="akzent_point_of_interest_options[api_key]" id="api_key" value="%s">',
@@ -94,8 +91,7 @@ class Settings
     );
   }
 
-  public function create_admin_page()
-  {
+  public function create_admin_page() {
     ?>
     <div class="wrap">
       <h2>Reiseinspirationen Konfiguration</h2>
@@ -130,8 +126,7 @@ class Settings
   }
 
 
-  public function api_key_sanitize($input)
-  {
+  public function api_key_sanitize($input) {
     $sanitary_values = array();
     if (isset($input['api_key'])) {
       if ($this->api_key_validate($input['api_key'])) {
